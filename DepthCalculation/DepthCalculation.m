b0474f37b4fbf40643715ae3315d555cb7db8a29
unknown <shijuntian20000126@126.com>
2021-05-25 21:05:42 +0800

    Upload the function of Depth Calculation

---
+++ DepthCalculation/DepthCalculation.m
@@ -0,0 +1,23 @@
+function z=DepthCalculation(a0,a1)
+% a0 stands for the pixel coordinate within cam0
+% a1 stands for the pixel coordinate within cam1
+clear;
+clc;
+Cam0toIMU=[0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975
+         0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768
+        -0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949
+         0.0, 0.0, 0.0, 1.0];
+Cam1toIMU=[0.0125552670891, -0.999755099723, 0.0182237714554, -0.0198435579556
+         0.999598781151, 0.0130119051815, 0.0251588363115, 0.0453689425024
+        -0.0253898008918, 0.0179005838253, 0.999517347078, 0.00786212447038
+         0.0, 0.0, 0.0, 1.0];
+% Cam0toIMU stands for the coordinate transformation matrix from Cam0 to
+% IMU
+B=sqrt((Cam0toIMU(1,4)-Cam1toIMU(1,4))^2+(Cam0toIMU(2,4)-Cam1toIMU(2,4))^2+(Cam0toIMU(3,4)-Cam1toIMU(3,4))^2);
+Cam0Intr=[458.654,457.296,367.215,248.375];
+Cam1Intr=[457.587,456.134,379.999,255.238];
+% To array above stands for [fx,fy,cx,cy]
+z=B*(Cam0Intr(1)+Cam1Intr(1))/2/(a0(1)-a1(1));
+end
+
+
